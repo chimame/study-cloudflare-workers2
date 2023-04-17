064040b37569617323face5d6003c665e3e0c19c
chimame <rito.tamata@gmail.com>
2023-04-18 02:51:16 +0900

    Add build and deploy to Cloud Run

---
+++ origin/script/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+CI_COMMIT_SHA=latest
+DATABASE_URL=$DATABASE_URL_FOR_MIGRATE
+
+# setting gcloud account
+gcloud auth login
+gcloud config set project $GCP_PROJECT_ID
+
+# build container image
+gcloud builds submit . --config=./cloudbuild.yml --substitutions=TAG_NAME=$CI_COMMIT_SHA
+
+# execute database migrate
+pnpx prisma migrate deploy
+
+# deploy application
+gcloud run deploy todo-nextjs \
+  --image gcr.io/$GCP_PROJECT_ID/my-origin-test:$CI_COMMIT_SHA \
+  --region asia-northeast1 \
+  --allow-unauthenticated \
+  --port '3000' \
+  --set-env-vars DATABASE_URL=$DATABASE_URL_FOR_CONNECTION_POOLING
